4f0fd841b8ab0e746388d8182ab2da5fa9f6eef8
savoy1211 <ryanlee250@gmail.com>
2020-05-21 13:47:30 -0700

    update main.py

--- lsci199/main.py
+++ lsci199/main.py
@@ -11,15 +11,17 @@ two_cities = download_gutenberg_text(url_prefix + "98-0.txt")
 moby_dick = download_gutenberg_text(url_prefix + "2701-0.txt")
 hard_times = download_gutenberg_text(url_prefix + "786-0.txt")
 
-text = pride_and_prejudice 
+text = pride_and_prejudice + two_cities + moby_dick
+# text = pride_and_prejudice
 model = AdditiveSmoothingNGramModel(text, n=2)
 h_words, h_wordset = [], []
 for i in range(1,6):
 	h_words_current, h_wordset_current = survey_text(model, model.tokens, i)
+	print("h_words",h_words_current,"h_wordset",h_wordset_current)
 	h_words.append(h_words_current)
 	h_wordset.append(h_wordset_current)
 
-d = {'window_size': [1, 2, 3, 4 ,5], 'h_words': h_words, 'h_wordset': h_wordset}
+d = {'window_size': [1, 2, 3, 4, 5], 'h_words': h_words, 'h_wordset': h_wordset}
 df = pd.DataFrame(data=d, dtype=np.float64)
-pd.DataFrame(df).to_csv("bigram_model_results.csv")
-print("Done! Created bigram_model_results.csv.")
+pd.DataFrame(df).to_csv("bigram_model_results_windows1to5_big.csv")
+print("Done! Created bigram_model_results_windows1to5_big.csv.")
