4812e4c4d13066032b355bb2a856c072e83ed4b2
savoy1211 <ubuntu@ip-172-31-56-112.ec2.internal>
2020-07-27 09:53:19 +0000

    add random training, ordered testing

--- lsci199/main_3grams_1to5_random_inbound.py
+++ lsci199/main_3grams_1to5_random_inbound.py
@@ -7,7 +7,8 @@ def each_text(training_text, training_text_names, testing_text, testing_text_nam
 	for i in range(len(training_text)):
 		start = time.time()
 		model = NGramModel(training_text[0], alpha=alpha, n=n, randomize_text=True, sentence_inbound=True, randomize_sentence_inbound=True, ordered_windows=False)
-		test = TestCorpus(testing_text[0], randomize_text=True, sentence_inbound=True, randomize_sentence_inbound=True, ordered_windows=False)
+		# test = TestCorpus(testing_text[0], randomize_text=True, sentence_inbound=True, randomize_sentence_inbound=True, ordered_windows=False)
+		test = TestCorpus(testing_text[0])
 		h_words, h_wordset = [], []
 		for j in range(1,6):
 			h_words_current, h_wordset_current = survey_text(model, test, j)
@@ -28,4 +29,5 @@ def each_text(training_text, training_text_names, testing_text, testing_text_nam
 # each_text([pride_and_prejudice+moby_dick+hard_times+two_cities],['mpht'], 3, alpha=0.25)
 # each_text([pride_and_prejudice+moby_dick+hard_times+two_cities],['mpht'], 3, alpha=0.50)
 
-each_text([mega_text_90],['train(mega_text_90)_'], [mega_text_10], ['_test(mega_text_10)'], 3, alpha=0.10)
+# each_text([mega_text_90],['train(mega_text_90)_'], [mega_text_10], ['_test(mega_text_10)'], 3, alpha=0.10)
+each_text([mega_text_10],['train(mega_text_10)_'], [pride_and_prejudice], ['pride_and_prejudice'], 3, alpha=0.10)
