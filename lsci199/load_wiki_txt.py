ff7ae96788e07659c63ee0d7b6e61fef39a822db
savoy1211 <ryanlee250@gmail.com>
2020-08-06 12:10:36 -0700

    modify load_wiki_txt

--- lsci199/load_wiki_txt.py
+++ lsci199/load_wiki_txt.py
@@ -10,3 +10,12 @@ with open('trwiki-20181001-corpus.xml.txt', 'r', encoding="utf-8") as current_fi
 
 turkish_90 = text[:int(l*.9/5)] 
 turkish_10 = text[int(l*.9):]
+
+with open('zhwiki-20181001-corpus.xml.txt', 'r', encoding="utf-8") as current_file:
+    text = current_file.read().encode('utf-8')[1:]
+    text = text.decode('utf-8', 'ignore')
+    l = len(text)
+    text = text[:int(len(text)/5)]
+
+chinese_90 = text[:int(l*.9/5)] 
+chinese_10 = text[int(l*.9):]
