1b25a5d262fc8d7c9d26404e97dd69c455e4f721
savoy1211 <ubuntu@ip-172-31-56-112.ec2.internal>
2020-08-06 19:04:07 +0000

    add new languages

---
+++ lsci199/load_wiki_txt.py
@@ -0,0 +1,12 @@
+import os
+import requests
+
+text = ''
+with open('trwiki-20181001-corpus.xml.txt', 'r', encoding="utf-8") as current_file:
+    text = current_file.read().encode('utf-8')[1:]
+    text = text.decode('utf-8', 'ignore')
+    l = len(text)
+    text = text[:int(len(text)/5)]
+
+turkish_90 = text[:int(l*.9/5)] 
+turkish_10 = text[int(l*.9):]
